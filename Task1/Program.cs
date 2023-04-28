073cd932a6d07e84e7dd2cc1db7313b864e42113
ArhyParh <zescoarty@gmail.com>
2023-04-28 06:40:40 +0300

    Решил 1 задачу

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+Main();
+void Main()
+{
+    Console.WriteLine("Ведите 5ти значное число:...");
+    int a = int.Parse(Console.ReadLine()!);
+    IsNumberPolin(a);
+}
+void IsNumberPolin(int a)
+{
+    if (IsNumberValid(a) == true)
+    {
+        if (a / 1000 == a / 10 % 10 + a % 10 * 10) { Console.WriteLine("Да!"); }
+        else { Console.WriteLine("Нет!"); }
+    }
+    else { Console.WriteLine("Вы ввели не 5ти значное число!"); }
+}
+bool IsNumberValid(int a)
+{
+    if (a < 100000 && a > 9999) { return true; }
+    else { return false; }
+}
